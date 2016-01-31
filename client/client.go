a5c47413ec205c2d0831b2838571e10dc3e1f085
Kyrill Zorin <zorink@zoringroup.com>
2016-01-31 17:45:13 +0000

    Added test client

--- client/client.go
+++ client/client.go
@@ -7,6 +7,27 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"io/ioutil"
 
 	"github.com/kyrillzorin/CS4032_Project/clientproxy"
 )﻿
+
+func init() {
+	err = clientproxy.Init()
+	if err != nil {
+		fmt.Println("Error opening DB:", err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	defer clientproxy.closeDB()
+	file := clientProxy.Open(test.jpg)
+	data, _ := ioutil.ReadFile("./test.jpeg")
+	file.Write(data)
+	file.Close()
+	
+	file = clientProxy.Open(test.jpg)
+	data = file.Read()
+	ioutil.WriteFile("output.jpg", data, 0644)
+}
